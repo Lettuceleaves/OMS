3ab234b29713f47cdab4241993fead075af1a4d7
LettuceLeaves <2878506229@qq.com>
2025-04-02 00:23:37 +0800

    add odd pom.xml files

---
+++ cloud-doc/src/test/java/org/example/clouddoc/CloudDocApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.clouddoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ scores-check/src/test/java/org/example/scorescheck/ScoresCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.scorescheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoresCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
