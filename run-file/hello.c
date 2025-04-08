71dd1156fe607096bed77c888a91b2a9c55234fd
LettuceLeaves <2878506229@qq.com>
2025-04-08 15:03:20 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	user-service/pom.xml

--- run-file/hello.c
+++ run-file/hello.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    int sum = a + b + c;
+    printf("a: %d, b: %d, c: %d\n", a, b, c);
+    printf("Sum: %d\n", sum);
     return 0;
 }
