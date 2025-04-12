fd7a5fe8ad21014dab3cb0487147afd2ff3b84fd
LettuceLeaves <2878506229@qq.com>
2025-04-12 17:33:07 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	user-service/pom.xml

--- run-file/aaa.c
+++ run-file/aaa.c
@@ -5,9 +5,9 @@ int main() {
     // 打印 0 到 1000项斐波那契数列， 注意溢出问题
     long a = 0, b = 1, c;
     printf("0\n");
-    for (int i = 1; i <= 10000; i++) {
+    for (int i = 1; i <= 1000; i++) {
         c = a + b;
-        if (c > 10000) {
+        if (c > 10000 {
             break;
         }
         printf("%ld\n", c);
